39d808cf0d370e34b2ce998b2e5a374ba5cfad46
munkkiMachine <arya.abawi@gmail.com>
2021-03-18 17:49:07 -0400

    updated tests for single observation prediction

--- src/tests/api/test_api.py
+++ src/tests/api/test_api.py
@@ -7,16 +7,13 @@ from starlette.testclient import TestClient
 from starlette.status import HTTP_200_OK, HTTP_422_UNPROCESSABLE_ENTITY
 from itertools import product
 
-n_features = Model().n_features
+n_features = Model().n_inputs
 
-#TODO: Require tests to ensure tensor size matches discriminator expectation
 
-@pytest.mark.parametrize("n_instances", range(11, 11))
-def test_predict(n_instances: int, test_client: TestClient):
-    fake_data = [[random.random() for _ in range(n_features)] for _ in range(n_instances)]
+def test_predict(test_client: TestClient):
+    fake_data = [random.random() for _ in range(n_features)]
     response = test_client.post("/predict", json={"data": fake_data})
     assert response.status_code == HTTP_200_OK
-    assert len(response.json()["data"]) == n_instances
 
 
 @pytest.mark.parametrize(
